642b2d6abfeb35aa8be9ecd28fcc55551625ad51
ifs-nuwglk <nuwan.gunaratne@ifs.com>
2022-11-12 20:21:44 +0530

    fix: update interfaces

---
+++ apps/matific/src/app/core/interfaces/common.interfaces.ts
@@ -0,0 +1,29 @@
+export interface IClass {
+  id: number;
+  students: string[];
+  name: string;
+}
+
+interface IAttempts {
+  weeks: string[];
+  values: number[];
+}
+
+export interface IActivities {
+  id: number;
+  content: string;
+  attempts: IAttempts;
+  student: string;
+  time: string;
+  skill: string;
+  type: string;
+}
+
+export interface IActivityResponse {
+  statusCode: string;
+  headers: {
+    'Content-Type': string;
+    'Access-Control-Allow-Origin': string;
+  };
+  body: string;
+}
