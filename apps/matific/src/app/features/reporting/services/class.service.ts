4d52809340cf6641767a16bd0bdfd93b2eda6dbc
ifs-nuwglk <nuwan.gunaratne@ifs.com>
2022-11-13 01:48:24 +0530

    fix: class service

--- apps/matific/src/app/features/reporting/services/class.service.ts
+++ apps/matific/src/app/features/reporting/services/class.service.ts
@@ -1,23 +1,28 @@
 import { Injectable } from '@angular/core';
 import { IClass } from '@matific/core/interfaces/common.interfaces';
-import { Observable, of, Subject } from 'rxjs';
+import { filter, map, Observable, of, Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NuguClassService {
-
-  private _classesChanged: Subject<IClass[]> = new Subject<IClass[]>();
-  classesChanged: Observable<IClass[]> = this._classesChanged;
+  private _classesChanged$: Subject<IClass[]> = new Subject<IClass[]>();
+  classesChanged$: Observable<IClass[]> = this._classesChanged$;
   private _classes: IClass[] = [];
 
   setClasses(classes: IClass[]): void {
     this._classes = classes;
-    this._classesChanged.next(this._classes.slice());
+    this._classesChanged$.next(this._classes.slice());
   }
 
   getClass$(className: string): Observable<IClass | undefined> {
     return of(this._classes.find((value) => value.name === className));
   }
 
+  getAllStudentsInClass$(className: string): Observable<string[]> {
+    return this.getClass$(className).pipe(
+      filter((value): value is IClass => !!value),
+      map((value) => value.students)
+    );
+  }
 }
