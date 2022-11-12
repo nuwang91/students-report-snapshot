82ca0f1d35fcdc308d0d036a95f76148a5af7ddf
ifs-nuwglk <nuwan.gunaratne@ifs.com>
2022-11-12 10:14:28 +0530

    feat: use table component

--- apps/matific/src/app/features/reporting/components/report/report.component.ts
+++ apps/matific/src/app/features/reporting/components/report/report.component.ts
@@ -1,9 +1,102 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NuguTableColumnInterface } from 'apps/matific/src/app/shared/components/table/table.component';
 
 interface City {
   name: string;
   code: string;
 }
+
+interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+  noble: boolean;
+}
+
+export const ELEMENT_DATA: PeriodicElement[] = [
+  {
+    position: 1,
+    name: 'Hydrogen',
+    weight: 1.0079,
+    symbol: 'H',
+    noble: false,
+  },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He', noble: true },
+  {
+    position: 3,
+    name: 'Lithium',
+    weight: 6.941,
+    symbol: 'Li',
+    noble: false,
+  },
+  {
+    position: 4,
+    name: 'Beryllium',
+    weight: 9.0122,
+    symbol: 'Be',
+    noble: false,
+  },
+  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B', noble: false },
+  {
+    position: 6,
+    name: 'Carbon',
+    weight: 12.0107,
+    symbol: 'C',
+    noble: false,
+  },
+  {
+    position: 7,
+    name: 'Nitrogen',
+    weight: 14.0067,
+    symbol: 'N',
+    noble: false,
+  },
+  {
+    position: 8,
+    name: 'Oxygen',
+    weight: 15.9994,
+    symbol: 'O',
+    noble: false,
+  },
+  {
+    position: 9,
+    name: 'Fluorine',
+    weight: 18.9984,
+    symbol: 'F',
+    noble: false,
+  },
+  {
+    position: 10,
+    name: 'Neon',
+    weight: 20.1797,
+    symbol: 'Ne',
+    noble: true,
+  },
+];
+
+export const columns: NuguTableColumnInterface[] = [
+  {
+    name: 'position',
+    title: 'position',
+  },
+  {
+    name: 'name',
+    title: 'Name',
+  },
+  {
+    name: 'weight',
+    title: 'Weight',
+  },
+  {
+    name: 'symbol',
+    title: 'Symbol',
+  },
+  {
+    name: 'noble',
+    title: 'Noble Gas',
+  },
+];
 
 @Component({
   selector: 'nugu-report',
@@ -15,6 +108,8 @@ export class NuguReportComponent {
 
   cities: City[];
 
+  data: PeriodicElement[] = ELEMENT_DATA;
+  columns: NuguTableColumnInterface[] = columns;
 
   constructor() {
     this.cities = [
--- apps/matific/src/app/features/reporting/components/report/report.module.ts
+++ apps/matific/src/app/features/reporting/components/report/report.module.ts
@@ -4,10 +4,16 @@ import { CommonModule } from '@angular/common';
 import { NuguReportComponent } from './report.component';
 import { NuguDropdownModule } from 'apps/matific/src/app/shared/components/dropdown/dropdown.module';
 import { NuguDatePickerModule } from 'apps/matific/src/app/shared/components/date-picker/date-picker.module';
+import { NuguTableModule } from 'apps/matific/src/app/shared/components/table/table.module';
 
 @NgModule({
   declarations: [NuguReportComponent],
-  imports: [CommonModule, NuguDropdownModule, NuguDatePickerModule],
+  imports: [
+    CommonModule,
+    NuguDropdownModule,
+    NuguDatePickerModule,
+    NuguTableModule,
+  ],
   exports: [NuguReportComponent],
 })
 export class NuguReportModule {}
