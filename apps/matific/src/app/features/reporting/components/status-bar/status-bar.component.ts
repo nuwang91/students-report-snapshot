6b3049e72f2577e772e1b2afcc7680fa090c36bf
ifs-nuwglk <nuwan.gunaratne@ifs.com>
2022-11-12 18:52:41 +0530

    feat: update status bar component

--- apps/matific/src/app/features/reporting/components/status-bar/status-bar.component.ts
+++ apps/matific/src/app/features/reporting/components/status-bar/status-bar.component.ts
@@ -1,4 +1,13 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
+
+import { ILegend } from '@matific/shared/components/legend/legend.component';
+import { IProgressBar } from '@matific/shared/components/progress-bar/progress-bar.component';
 
 @Component({
   selector: 'nugu-status-bar',
@@ -6,5 +15,21 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./status-bar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NuguStatusBarComponent {
+export class NuguStatusBarComponent implements OnChanges {
+  @Input()
+  values: IProgressBar[] = [];
+
+  _legendValues: ILegend[] = [];
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (!!changes['values'] && !!changes['values'].currentValue) {
+      this._legendValues = this.values.map((value) => {
+        return {
+          status: value.status,
+          color: value.color,
+          percentage: value.percentage,
+        };
+      });
+    }
+  }
 }
--- apps/matific/src/app/features/reporting/components/status-bar/status-bar.module.ts
+++ apps/matific/src/app/features/reporting/components/status-bar/status-bar.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NuguStatusBarComponent } from './status-bar.component';
+import { NuguProgressBarModule } from '@matific/shared/components/progress-bar/progress-bar.module';
+import { NuguLegendModule } from '@matific/shared/components/legend/legend.module';
 
 @NgModule({
   declarations: [NuguStatusBarComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    NuguProgressBarModule,
+    NuguLegendModule
   ],
   exports: [NuguStatusBarComponent]
 })
