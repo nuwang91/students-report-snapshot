17a76ac97280098ef9e84132d658f42136841f69
ifs-nuwglk <nuwan.gunaratne@ifs.com>
2022-11-12 09:27:12 +0530

    feat: added report to routing

--- apps/matific/src/app/app-routing.module.ts
+++ apps/matific/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { NuguAuthenticationComponent } from './core/components/authentication/authentication.component';
+import { NuguReportComponent } from './features/reporting/components/report/report.component';
 
 const routes: Routes = [
   { path: 'auth', component: NuguAuthenticationComponent },
+  { path: 'report', component: NuguReportComponent },
 ];
 
 @NgModule({
