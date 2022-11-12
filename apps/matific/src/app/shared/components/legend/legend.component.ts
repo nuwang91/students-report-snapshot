d9ac0961b984e49a67505734fb155228fa11e8e0
ifs-nuwglk <nuwan.gunaratne@ifs.com>
2022-11-12 13:06:32 +0530

    feat: created legend component

---
+++ apps/matific/src/app/shared/components/legend/legend.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'nugu-legend',
+  templateUrl: './legend.component.html',
+  styleUrls: ['./legend.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NuguLegendComponent {}
---
+++ apps/matific/src/app/shared/components/legend/legend.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NuguLegendComponent } from './legend.component';
+
+@NgModule({
+  declarations: [NuguLegendComponent],
+  imports: [CommonModule],
+  exports: [NuguLegendComponent],
+})
+export class NuguLegendModule {}
