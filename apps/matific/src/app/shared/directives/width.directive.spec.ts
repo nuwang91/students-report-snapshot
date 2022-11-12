b7f67a842db78e6c44c5e964684c8b23ea96383d
ifs-nuwglk <nuwan.gunaratne@ifs.com>
2022-11-13 01:52:20 +0530

    refactor: deleted unused directives

--- apps/matific/src/app/shared/directives/width.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NuguWidthDirective } from './width.directive';
-
-describe('NuguWidthDirective', () => {
-  it('should create an instance', () => {
-    const directive = new NuguWidthDirective();
-    expect(directive).toBeTruthy();
-  });
-});
