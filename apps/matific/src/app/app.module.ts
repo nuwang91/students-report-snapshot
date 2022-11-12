a7aca730812d1e20547707985b94e8627f197403
ifs-nuwglk <nuwan.gunaratne@ifs.com>
2022-11-12 09:12:19 +0530

    feat: updated app module and component

--- apps/matific/src/app/app.module.ts
+++ apps/matific/src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { NuguAppRoutingModule } from './app-routing.module';
+import { NuguAuthenticationModule } from './core/components/authentication/authentication.module';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    NuguAppRoutingModule,
+    NuguAuthenticationModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
