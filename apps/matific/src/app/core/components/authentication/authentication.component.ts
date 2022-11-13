7186cd9282179d0558d81027d61754afa1c3f3c2
ifs-nuwglk <nuwan.gunaratne@ifs.com>
2022-11-13 16:25:28 +0530

    refactor: refac auth

--- apps/matific/src/app/core/components/authentication/authentication.component.ts
+++ apps/matific/src/app/core/components/authentication/authentication.component.ts
@@ -4,7 +4,10 @@ import { Router } from '@angular/router';
 
 import { Observable } from 'rxjs';
 
-import { AuthResponseData, NuguAuthenticationService } from '../../services/authentication.service';
+import {
+  AuthResponseData,
+  NuguAuthenticationService,
+} from '../../services/authentication.service';
 
 @Component({
   selector: 'nugu-auth',
@@ -13,9 +16,9 @@ import { AuthResponseData, NuguAuthenticationService } from '../../services/auth
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NuguAuthenticationComponent {
-  isLoginMode = true;
+  _isLoginMode = true;
 
-  userForm: FormGroup = new FormGroup({
+  _userForm: FormGroup = new FormGroup({
     user: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [
       Validators.required,
@@ -23,26 +26,26 @@ export class NuguAuthenticationComponent {
     ]),
   });
 
-  onSwitchMode() {
-    this.isLoginMode = !this.isLoginMode;
-  }
-
   constructor(
     private authenticationService: NuguAuthenticationService,
     private router: Router
   ) {}
 
-  login() {
-    if (!this.userForm.valid) {
+  _onSwitchMode() {
+    this._isLoginMode = !this._isLoginMode;
+  }
+
+  _login() {
+    if (!this._userForm.valid) {
       return;
     }
 
-    const email = this.userForm.value.user;
-    const password = this.userForm.value.password;
+    const email = this._userForm.value.user;
+    const password = this._userForm.value.password;
 
     let authObs: Observable<AuthResponseData>;
 
-    if (this.isLoginMode) {
+    if (this._isLoginMode) {
       authObs = this.authenticationService.login(email, password);
     } else {
       authObs = this.authenticationService.signup(email, password);
@@ -51,13 +54,13 @@ export class NuguAuthenticationComponent {
     authObs.subscribe({
       next: (resData) => {
         console.log(resData);
-        this.router.navigate(['/report'])
+        this.router.navigate(['/report']);
       },
       error: (errorMessage) => {
         console.log(errorMessage);
       },
     });
 
-    this.userForm.reset();
+    this._userForm.reset();
   }
 }
